be76689bb82384dfd70c4cb868984f0ec889e42f
kizami <c0b2204952@edu.teu.ac.jp>
2023-10-03 17:21:36 +0900

    演習１途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,8 +3,8 @@ import sys
 import pygame as pg
 
 
-
 WIDTH, HEIGHT = 1600, 900
+
 
 delta = {#練習３:移動量辞書
     pg.K_UP: (0, -5),
@@ -12,6 +12,7 @@ delta = {#練習３:移動量辞書
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
 }
+
 
 
 def check_bound(obj_rct: pg.Rect):
@@ -27,15 +28,20 @@ def check_bound(obj_rct: pg.Rect):
         tate = False
     return yoko,tate
 
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
+    key_lst = pg.key.get_pressed()
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = (900,400)
+    kk_img2 = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct2 = kk_img2.get_rect()
+    kk_rct2.center = (900,400)
     """爆弾"""
     bd_img = pg.Surface((20, 20))#爆弾surfaceの作成
     bd_img.set_colorkey((0, 0, 0))
@@ -44,14 +50,16 @@ def main():
     x, y = random.randint(0,WIDTH),random.randint(0,HEIGHT)
     bd_rct.center = (x, y)#練習1　Rectにランダムな座標を設定する
     vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
         if kk_rct.colliderect(bd_rct):
+
             print("Game Over")
             return
 
@@ -60,10 +68,22 @@ def main():
         """こうかとん"""
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+        muki = {
+            (-5,0):pg.transform.rotozoom(kk_img, 0, 2.0),
+            (-5,-5):pg.transform.rotozoom(kk_img, 45, 2.0),
+            (0,-5):pg.transform.rotozoom(kk_img, 90, 2.0),
+            (+5,-5):pg.transform.rotozoom(kk_img2, 90, 2.0),
+            (+5,0):pg.transform.rotozoom(kk_img2, 45, 2.0),
+            (+5,+5):pg.transform.rotozoom(kk_img2, 0, 2.0),
+            (0,+5):pg.transform.rotozoom(kk_img2, -45, 2.0),
+            (-5,+5):pg.transform.rotozoom(kk_img2, -90, 2.0)
+        }
         for key ,mv in delta.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+            screen.blit(muki(sum_mv),kk_rct) 
+            
         kk_rct.move_ip(sum_mv[0],sum_mv[1])
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
