14b7aef3f2961505b20d0f2d128eabe140425e3c
kizami <c0b2204952@edu.teu.ac.jp>
2023-10-03 14:46:34 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,13 +6,24 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {#練習３:移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900,400)
+    """爆弾"""
     bd_img = pg.Surface((20, 20))#爆弾surfaceの作成
     bd_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
@@ -29,12 +40,22 @@ def main():
                 return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key ,mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+        """爆弾"""
         bd_rct.move_ip(vx,vy)
         screen.blit(bd_img,bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
